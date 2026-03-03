118867b770f4da69c92d5b33ca64c257a9604e0d
Matti Schwartz <matti.schwartz@alpininsight.com>
2026-03-03 18:30:13 +0100

    refactor: remove hardcoded list of components from smoke tests

--- tests/smoke/test_smoke.py
+++ tests/smoke/test_smoke.py
@@ -10,6 +10,7 @@ from http import HTTPStatus
 import pytest
 from django.test import Client
 from django.urls import reverse
+from insight_ui.component_details.component_context import CONTEXT_BUILDERS
 
 
 @pytest.mark.smoke
@@ -45,56 +46,7 @@ def test_storybook_urls_responds_ok(viewname: str, client: Client) -> None:  # n
 
 @pytest.mark.smoke
 @pytest.mark.django_db
-@pytest.mark.parametrize(
-    "component_name",
-    [
-        "page_header",
-        "article",
-        "hero",
-        "navbar",
-        "sidebar",
-        "footer",
-        "breadcrumb",
-        "step_bar",
-        "bullet_point_list",
-        "accordion",
-        "tabs",
-        "button",
-        "input_field",
-        "checkbox",
-        "checkbox_group",
-        "dropdown",
-        "radio_group",
-        "range_slider",
-        "toggle_button",
-        "select",
-        "multiselect",
-        "chat",
-        "alert",
-        "modal",
-        "popover",
-        "tooltip",
-        "code_block",
-        "differentiator",
-        "progress_bar",
-        "geo_map",
-        "chart",
-        "live_content",
-        "web_socket",
-        "infinite_scroll",
-        "pagination",
-        "table",
-        "generic_filter",
-        "search_bar",
-        "query_builder",
-        "card",
-        "card_carousel",
-        "image_carousel",
-        "3D_carousel",
-        "toggle_view",
-        "form",
-    ],
-)
+@pytest.mark.parametrize("component_name", CONTEXT_BUILDERS.keys())
 def test_component_urls_responds_ok(component_name: str, client: Client) -> None:  # noqa: ANN001
     """Basic smoke test for the component pages."""
     response = client.get(reverse("component_detail_page_view", kwargs={"component_name": component_name}))
@@ -105,58 +57,7 @@ def test_component_urls_responds_ok(component_name: str, client: Client) -> None
 @pytest.mark.smoke
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    "component_name",
-    [
-        "page_header",
-        "article",
-        "hero",
-        "navbar",
-        "sidebar",
-        "footer",
-        "breadcrumb",
-        "step_bar",
-        "bullet_point_list",
-        "accordion",
-        "accordion_exclusive",
-        "tabs",
-        "button",
-        "outline_button",
-        "button_sizes",
-        "input_field",
-        "checkbox",
-        "checkbox_group",
-        "dropdown",
-        "radio_group",
-        "radio_block",
-        "range_slider",
-        "toggle_button",
-        "select",
-        "multiselect",
-        "chat",
-        "alert",
-        "modal",
-        "popover",
-        "tooltip",
-        "code_block",
-        "differentiator",
-        "progress_bar",
-        "geo_map",
-        "chart",
-        "live_content",
-        "web_socket",
-        "infinite_scroll",
-        "pagination",
-        "table",
-        "generic_filter",
-        "search_bar",
-        "query_builder",
-        "card",
-        "card_carousel",
-        "image_carousel",
-        "3D_carousel",
-        "toggle_view",
-        "form",
-    ],
+    "component_name", [*CONTEXT_BUILDERS.keys(), "accordion_exclusive", "outline_button", "button_sizes"]
 )
 def test_component_demo_urls_responds_ok(component_name: str, client: Client) -> None:  # noqa: ANN001
     """Basic smoke test for the component demo pages."""
